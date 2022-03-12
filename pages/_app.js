b242c298c4e315df06ec5ffafdd4e773a36260d0
kvoncode <kvoncode@gmail.com>
2022-03-12 14:10:31 +0900

    add: antd styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+import "antd/dist/antd.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
